54d386862c95d03853fa37235d3c20daaa9bc991
patrick <patrick.zierahn@student.kit.edu>
2023-10-15 20:54:28 +0200

    Added credentials back in

--- server/server_init.go
+++ server/server_init.go
@@ -3,6 +3,8 @@ package server
 import (
 	"context"
 	_ "embed"
+	auth "golang.org/x/oauth2/google"
+	"google.golang.org/api/option"
 	"google.golang.org/api/sheets/v4"
 	"log"
 )
@@ -12,20 +14,21 @@ var sheetsService *sheets.Service
 func init() {
 	ctx := context.Background()
 
-	//params := auth.CredentialsParams{
-	//	Scopes: []string{
-	//		sheets.SpreadsheetsReadonlyScope,
-	//	},
-	//	Subject: "patrick.zierahn@fuks.org",
-	//}
-	//
-	//credentials, err := auth.FindDefaultCredentialsWithParams(ctx, params)
-	//if err != nil {
-	//	log.Fatalln(err)
-	//}
+	params := auth.CredentialsParams{
+		Scopes: []string{
+			sheets.SpreadsheetsReadonlyScope,
+		},
+		Subject: "patrick.zierahn@fuks.org",
+	}
 
-	sheet, err := sheets.NewService(ctx) //option.WithCredentials(credentials),
+	credentials, err := auth.FindDefaultCredentialsWithParams(ctx, params)
+	if err != nil {
+		log.Fatalln(err)
+	}
 
+	sheet, err := sheets.NewService(ctx,
+		option.WithCredentials(credentials),
+	)
 	if err != nil {
 		log.Fatalf("Unable to retrieve Sheets client: %v", err)
 	}
