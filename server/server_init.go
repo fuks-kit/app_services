a89e431a3bd3bc36ca0455127fb081f12625fe41
patrick <patrick.zierahn@student.kit.edu>
2023-10-15 23:14:06 +0200

    Added debug logs

--- server/server_init.go
+++ server/server_init.go
@@ -3,6 +3,7 @@ package server
 import (
 	"context"
 	_ "embed"
+	"encoding/json"
 	auth "golang.org/x/oauth2/google"
 	"google.golang.org/api/option"
 	"google.golang.org/api/sheets/v4"
@@ -11,6 +12,18 @@ import (
 
 var sheetsService *sheets.Service
 
+type Debug struct {
+	Type                    string `json:"type"`
+	ProjectId               string `json:"project_id"`
+	ClientEmail             string `json:"client_email"`
+	ClientId                string `json:"client_id"`
+	AuthUri                 string `json:"auth_uri"`
+	TokenUri                string `json:"token_uri"`
+	AuthProviderX509CertUrl string `json:"auth_provider_x509_cert_url"`
+	ClientX509CertUrl       string `json:"client_x509_cert_url"`
+	UniverseDomain          string `json:"universe_domain"`
+}
+
 func init() {
 	ctx := context.Background()
 
@@ -18,13 +31,19 @@ func init() {
 		Scopes: []string{
 			sheets.SpreadsheetsReadonlyScope,
 		},
-		Subject: "113222746783594416669",
+		Subject: "patrick.zierahn@fuks.org",
 	}
 
 	credentials, err := auth.FindDefaultCredentialsWithParams(ctx, params)
 	if err != nil {
 		log.Fatalln(err)
 	}
+
+	log.Printf("ProjectID: %s", credentials.ProjectID)
+
+	var deb Debug
+	json.Unmarshal(credentials.JSON, &deb)
+	log.Printf("########: %+v", deb)
 
 	sheet, err := sheets.NewService(ctx,
 		option.WithCredentials(credentials),
