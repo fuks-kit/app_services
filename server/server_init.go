4e2bb0a89b243d854f87881cd1b176323da200bc
patrick <patrick.zierahn@student.kit.edu>
2023-10-16 00:18:50 +0200

    Clean up

--- server/server_init.go
+++ server/server_init.go
@@ -7,22 +7,9 @@ import (
 	"google.golang.org/api/option"
 	"google.golang.org/api/sheets/v4"
 	"log"
-	"os"
 )
 
 var sheetsService *sheets.Service
-
-type Debug struct {
-	Type                    string `json:"type"`
-	ProjectId               string `json:"project_id"`
-	ClientEmail             string `json:"client_email"`
-	ClientId                string `json:"client_id"`
-	AuthUri                 string `json:"auth_uri"`
-	TokenUri                string `json:"token_uri"`
-	AuthProviderX509CertUrl string `json:"auth_provider_x509_cert_url"`
-	ClientX509CertUrl       string `json:"client_x509_cert_url"`
-	UniverseDomain          string `json:"universe_domain"`
-}
 
 func init() {
 	ctx := context.Background()
@@ -36,15 +23,10 @@ func init() {
 		Subject: "patrick.zierahn@fuks.org",
 	}
 
-	log.Printf("######## GOOGLE_APPLICATION_CREDENTIALS: %s", os.Getenv("GOOGLE_APPLICATION_CREDENTIALS"))
-
 	credentials, err := auth.FindDefaultCredentialsWithParams(ctx, params)
 	if err != nil {
 		log.Fatalln(err)
 	}
-
-	log.Printf("######## credentials.ProjectID: %s", credentials.ProjectID)
-	log.Printf("######## JSON len: %d", len(credentials.JSON))
 
 	sheet, err := sheets.NewService(ctx,
 		option.WithCredentials(credentials),
