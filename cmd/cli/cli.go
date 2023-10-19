e926faf7c7b2efbbfc8fc417c91e4f6e151aa789
patrick <patrick.zierahn@student.kit.edu>
2023-10-19 12:59:21 +0200

    Update cli

--- cmd/cli/cli.go
+++ cmd/cli/cli.go
@@ -5,31 +5,44 @@ import (
 	"crypto/tls"
 	"crypto/x509"
 	"encoding/json"
+	"flag"
 	pb "github.com/fuks-kit/app_services/proto"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials"
 	"google.golang.org/grpc/credentials/insecure"
 	"google.golang.org/protobuf/types/known/emptypb"
 	"log"
+	"slices"
 )
 
-const (
-	host       = "app-services-befklaxdqa-ey.a.run.app:443"
-	isInsecure = false
-	//host       = "localhost:30336"
-	//isInsecure = true
+var (
+	host       = flag.String("host", "localhost:30336", "host:port of gRPC server")
+	isInsecure = flag.Bool("insecure", true, "connect without TLS")
 )
+
+var validActions = []string{
+	"get_events",
+	"get_projects",
+	"get_karlsruher_transfers",
+}
 
 func main() {
 
 	log.SetFlags(log.LstdFlags | log.Lshortfile)
+	flag.Parse()
 
-	var opts []grpc.DialOption
-	if host != "" {
-		opts = append(opts, grpc.WithAuthority(host))
+	actions := flag.Args()
+
+	if len(actions) == 0 {
+		log.Fatalf("no action specified: %v", validActions)
 	}
 
-	if isInsecure {
+	var opts []grpc.DialOption
+	if *host != "" {
+		opts = append(opts, grpc.WithAuthority(*host))
+	}
+
+	if *isInsecure {
 		opts = append(opts, grpc.WithTransportCredentials(insecure.NewCredentials()))
 	} else {
 		// Note: On the Windows platform, use of x509.SystemCertPool() requires
@@ -46,7 +59,7 @@ func main() {
 		opts = append(opts, grpc.WithTransportCredentials(cred))
 	}
 
-	client, err := grpc.Dial(host, opts...)
+	client, err := grpc.Dial(*host, opts...)
 	if err != nil {
 		log.Fatalf("did not connect: %v", err)
 	}
@@ -60,35 +73,41 @@ func main() {
 	// Get Events
 	//
 
-	events, err := appServices.GetEvents(ctx, &emptypb.Empty{})
-	if err != nil {
-		log.Fatalf("could not get events: %v", err)
-	}
+	if slices.Contains(validActions, "get_events") {
+		events, err := appServices.GetEvents(ctx, &emptypb.Empty{})
+		if err != nil {
+			log.Fatalf("could not get events: %v", err)
+		}
 
-	byt, _ := json.MarshalIndent(events, "", "  ")
-	log.Printf("GetEvents: %s", byt)
+		byt, _ := json.MarshalIndent(events, "", "  ")
+		log.Printf("GetEvents: %s", byt)
+	}
 
 	//
 	// Get Projects
 	//
 
-	projects, err := appServices.GetProjects(ctx, &emptypb.Empty{})
-	if err != nil {
-		log.Fatalf("could not get events: %v", err)
-	}
+	if slices.Contains(validActions, "get_projects") {
+		projects, err := appServices.GetProjects(ctx, &emptypb.Empty{})
+		if err != nil {
+			log.Fatalf("could not get events: %v", err)
+		}
 
-	byt, _ = json.MarshalIndent(projects, "", "  ")
-	log.Printf("GetProjects: %s", byt)
+		byt, _ := json.MarshalIndent(projects, "", "  ")
+		log.Printf("GetProjects: %s", byt)
+	}
 
 	//
 	// Get Kalrsruhe Transfers
 	//
 
-	kts, err := appServices.GetKarlsruherTransfers(ctx, &emptypb.Empty{})
-	if err != nil {
-		log.Fatalf("could not get events: %v", err)
-	}
+	if slices.Contains(validActions, "get_karlsruher_transfers") {
+		kts, err := appServices.GetKarlsruherTransfers(ctx, &emptypb.Empty{})
+		if err != nil {
+			log.Fatalf("could not get events: %v", err)
+		}
 
-	byt, _ = json.MarshalIndent(kts, "", "  ")
-	log.Printf("GetKarlsruherTransfers: %s", byt)
+		byt, _ := json.MarshalIndent(kts, "", "  ")
+		log.Printf("GetKarlsruherTransfers: %s", byt)
+	}
 }
